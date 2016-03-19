95a2fec8b4d777f169f65256768081806987cfc9
Dean Connolly <deanconnolly1995@gmail.com>
2016-03-19 13:07:39 +1000

    Minor changes
    Added FontAwesome 4.5.0 icons to some elements of navbar.
    
    editArticle.php is currently not working right - we’re trying to fix
    before next commit, but for now, enjoy the new editor design we’ve
    implemented! :)

--- templates/admin/editArticle.php
+++ templates/admin/editArticle.php
@@ -2,42 +2,56 @@
 
     <body>
         <?php include "inc/navbar.php"; ?>
-            <div id="container" class="container">
-                <h1><?php echo $results['pageTitle']?></h1>
+            <div class="container">
                 <form action="admin.php?action=<?php echo $results['formAction']?>" method="post">
-                    <div class="col-md-6">
-                        <input type="hidden" name="articleId" value="<?php echo $results['article']->id ?>" />
-                        <?php if ( isset( $results['errorMessage'] ) ) { ?>
-                            <div class="errorMessage">
-                                <?php echo $results['errorMessage'] ?>
-                            </div>
-                            <?php } ?>
 
-                                <label for="title">Article Title</label>
+                    <?php if ( isset( $results['errorMessage'] ) ) { ?>
+                        <div class="alert alert-dismissible alert-info">
+                            <button type="button" class="close" data-dismiss="alert">X</button>
+                            <?php echo $results['errorMessage'] ?>
+                        </div>
+                        <?php } ?>
+
+                            <!-- TITLE DIV !-->
+                            <div class="col-md-4">
+                                <label for="title">Post Title</label>
                                 <input type="text" name="title" id="title" placeholder="Name of the article" required autofocus maxlength="255" value="<?php echo htmlspecialchars( $results['article']->title )?>" class="form-control" />
+                            </div>
 
-                                <label for="summary">Article Summary</label>
-                                <textarea name="summary" id="summary" placeholder="Brief description of the article" required maxlength="1000" style="height: 5em;" class="form-control">
-                                    <?php echo htmlspecialchars( $results['article']->summary )?>
-                                </textarea>
+                            <!-- SUMMARY DIV !-->
+                            <div class="col-md-4">
+                                <label for="summary">Summary</label>
+                                <input type="text" name="summary" id="summary" placeholder="Summary of the post" required maxlength="1000" class="form-control" value="<?php echo htmlspecialchars( $results['article']->summary )?>">
+                            </div>
 
+                            <!-- DATE DIV !-->
+                            <div class="col-md-4">
                                 <label for="publicationDate">Publication Date</label>
                                 <input type="date" name="publicationDate" id="publicationDate" placeholder="YYYY-MM-DD" required maxlength="10" value="<?php echo $results['article']->publicationDate ? date( " Y-m-d ", $results['article']->publicationDate ) : " " ?>" class="form-control" />
+                            </div>
+
+                            <!-- CONTENT DIV !-->
+                            <div class="col-md-12">
+                                <label for="content">Content</label>
+                                <textarea name="content" id="content" placeholder="The HTML content of the article" required maxlength="50000" style="height: 30em;" class="form-control">
+                                    <?php echo htmlspecialchars( $results['article']->content )?>
+                                </textarea>
+                            </div>
+
+                            <!-- POST ID AND ACTION BUTTONS DIV !-->
+                            <div class="col-md-12">
+                                <input type="hidden" name="articleId" value="<?php echo $results['article']->id ?>" />
                                 <br />
-                                <div class="buttons">
-                                    <input type="submit" name="saveChanges" value="Save Changes" class="btn btn-success" />
-                                    <input type="submit" formnovalidate name="cancel" value="Cancel" class="btn btn-warning" />
-                                    <?php if ( $results['article']->id ) { ?>
-                                        <a href="admin.php?action=deleteArticle&amp;articleId=<?php echo $results['article']->id ?>" onclick="return confirm('Delete This Article?')" class="btn btn-danger">Delete This Article</a>
-                                        <?php } ?>
-                                </div>
-                    </div>
-                    <div class="col-md-6">
-                        <label for="content">Article Content</label>
-                        <textarea name="content" id="content" placeholder="The HTML content of the article" required maxlength="50000" style="height: 30em;" class="form-control">
-                            <?php echo htmlspecialchars( $results['article']->content )?>
-                        </textarea>
-                    </div>
+                                <label>Actions: </label>
+                                <input type="submit" name="saveChanges" value="Save Changes" class="btn btn-success btn-block" />
+                                <br />
+                                <input type="submit" formnovalidate name="cancel" value="Cancel" class="btn btn-warning btn-block" />
+                                <?php if ( $results['article']->id ) { ?>
+                                    <a href="admin.php?action=deleteArticle&amp;articleId=<?php echo $results['article']->id ?>" onclick="return confirm('Delete This Article?')" class="btn btn-danger btn-block">Delete Post</a>
+                                    <?php } ?>
+
+                            </div>
                 </form>
             </div>
+            <?php include "templates/include/stickyfooter.php"; ?>
     </body>
--- templates/admin/inc/navbar.php
+++ templates/admin/inc/navbar.php
@@ -8,11 +8,11 @@
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="admin.php?action=newArticle">New Post</a></li>
+                <li><a href="admin.php?action=newArticle"><i class="fa fa-plus"></i> New Post</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Hi there, <?php echo htmlspecialchars( $_SESSION['username']) ?>.</a></li>
-                <li><a href="admin.php?action=logout">Log out</a></li>
+                <li><a href="admin.php?action=logout"><i class="fa fa-power-off"></i> Log out</a></li>
             </ul>
         </div>
     </div>
